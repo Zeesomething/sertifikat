e03b4c8f24fdb4661a47aa3128c22297e564a5aa
Bisma faza <bismafaza@gmail.com>
2024-09-05 10:32:53 +0700

    active indication

--- resources/views/backend/sidebar.blade.php
+++ resources/views/backend/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
         <a href="{{ route('home') }}" class="app-brand-link">
-            <img src="{{ asset('assets/img/icons/logo-bartech.png') }}" style="display: block;  max-width: 100%; margin: auto; z-index: 10" alt="">
+            <img src="{{ asset('assets/img/icons/logo-bartech.png') }}" style="display: block; max-width: 100%; margin: auto; z-index: 10" alt="">
         </a>
 
         <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
@@ -14,7 +14,7 @@
 
     <ul class="menu-inner py-1">
         <!-- Dashboard -->
-        <li class="menu-item active">
+        <li class="menu-item {{ request()->routeIs('home') ? 'active' : '' }}">
             <a href="{{ route('home') }}" class="menu-link">
                 <i class="menu-icon tf-icons bx bx-home-circle"></i>
                 <div data-i18n="Analytics">Dashboard</div>
@@ -24,28 +24,24 @@
         <li class="menu-header small text-uppercase">
             <span class="menu-header-text">Pages</span>
         </li>
-        <li class="menu-item">
+        <li class="menu-item {{ request()->routeIs('training.index') || request()->routeIs('sertifikat.index') ? 'active open' : '' }}">
             <a href="javascript:void(0);" class="menu-link menu-toggle">
                 <i class='menu-icon tf-icons bx bxs-data'></i>
                 <div data-i18n="Account Settings">Data Table</div>
             </a>
-            <ul class="menu-sub">
-                <li class="menu-item">
+            <ul class="menu-sub {{ request()->routeIs('training.index') || request()->routeIs('sertifikat.index') ? 'show' : '' }}">
+                <li class="menu-item {{ request()->routeIs('training.index') ? 'active' : '' }}">
                     <a href="{{ route('training.index') }}" class="menu-link">
                         <div data-i18n="Account">Pelatihan</div>
                     </a>
                 </li>
-            </ul>
-            <ul class="menu-sub">
-                <li class="menu-item">
+                <li class="menu-item {{ request()->routeIs('sertifikat.index') ? 'active' : '' }}">
                     <a href="{{ route('sertifikat.index') }}" class="menu-link">
                         <div data-i18n="Account">Sertifikat</div>
                     </a>
                 </li>
             </ul>
         </li>
-
-
     </ul>
 </aside>
 <!-- / Menu -->
