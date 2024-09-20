682441a870c51888cfd3af255dfc50d1b5cfb5e5
Moh Bisma Targaryen <147211527+EnjoyUrLife69@users.noreply.github.com>
2024-09-20 16:18:47 +0700

    update

--- app/Http/Controllers/SertifikatController.php
+++ app/Http/Controllers/SertifikatController.php
@@ -146,6 +146,7 @@ class SertifikatController extends Controller
         return $date->format('j') . $suffix;
     }
 
+    
     // Metode untuk mencetak sertifikat berdasarkan ID
     public function printCertificate($id)
     {
