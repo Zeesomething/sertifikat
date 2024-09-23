bc0ccae95d1b4dfbde2a9367e9db3fec2a4eb001
Moh Bisma Targaryen <147211527+EnjoyUrLife69@users.noreply.github.com>
2024-09-23 16:14:35 +0700

    up date

--- resources/views/sertifikat/index.blade.php
+++ resources/views/sertifikat/index.blade.php
@@ -117,7 +117,7 @@
                     <!-- Content -->
 
                     <div class="container-xxl flex-grow-1 container-p-y">
-                        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Data Table /</span> <b
+                        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Tabel Data  /</span> <b
                                 data-aos="fade-left" data-aos-duration="500">Sertifikat</b>
                         </h4>
 
--- resources/views/training/index.blade.php
+++ resources/views/training/index.blade.php
@@ -115,8 +115,8 @@
                     <!-- Content -->
 
                     <div class="container-xxl flex-grow-1 container-p-y">
-                        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Data Table /</span> <b
-                                data-aos="fade-left" data-aos-duration="500">Training</b>
+                        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Tabel Data /</span> <b
+                                data-aos="fade-left" data-aos-duration="500">Pelatihan</b>
                         </h4>
 
                         <!-- Bordered Table -->
